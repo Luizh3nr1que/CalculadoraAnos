53bb147d40dccc78583e0e33d358031f5b325c85
luizh3nrique <luizh3nrique00@gmail.com>
2023-06-09 13:27:29 -0300

    Signed-off-by: luizh3nrique <luizh3nrique00@gmail.com>

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
-const calculatorBtn = document.querySelector('.calcular');
-const inputDate = document.getElementById('date');
 const resultado = document.querySelector('.resultado');
 
 
-
 function calcularIdade() {
-    const dataSelecionada = new Date(inputDate.value);
+    const inputType = document.getElementById("inputType").value;
+    const resultado = document.querySelector(".resultado");
+
+    let dataSelecionada;
+
+    if (inputType === "selector") {
+        dataSelecionada = new Date(document.getElementById("selectorInput").value);
+    } else if (inputType === "text") {
+        const dateInputValue = document.getElementById("textInput").value;
+        const [day, month, year] = dateInputValue.split("/");
+        dataSelecionada = new Date(`${year}-${month}-${day}`);
+    }
+
     const dataAtual = new Date();
-
-
-    const diferencaMilissegundos = dataAtual - dataSelecionada
+    const diferencaMilissegundos = dataAtual - dataSelecionada;
     const idadeEmAnos = Math.floor(diferencaMilissegundos / (1000 * 60 * 60 * 24 * 365.25));
 
     if (idadeEmAnos >= 0) {
-        resultado.innerHTML = `Você tem ${idadeEmAnos} anos.`
+        resultado.innerHTML = `Você tem ${idadeEmAnos} anos.`;
         resultado.style.display = 'flex';
         clearButton.style.display = 'block';
     } else {
-        resultado.innerHTML = `Entrada invalida`
+        resultado.innerHTML = 'Entrada inválida';
+        resultado.style.display = 'flex';
     }
 }
 
@@ -27,15 +35,36 @@ document.addEventListener("keypress", function (event) {
     }
 });
 
+const calculatorBtn = document.querySelector(".calcular");
 calculatorBtn.addEventListener('click', calcularIdade);
 
 
 function clearIdade() {
-    inputDate.value = '';
+    var inputSelector = document.getElementById("selectorInput");
+    var inputText = document.getElementById("textInput");
+
     resultado.style.display = 'none';
     clearButton.style.display = 'none';
+    inputSelector.value = "";
+    inputText.value = "";
 }
 
 const clearButton = document.querySelector('.clear');
 
 clearButton.addEventListener('click', clearIdade);
+
+
+
+function toggleInput() {
+    var inputType = document.getElementById("inputType").value;
+    var seletorData = document.querySelector('.seletor')
+    var texto = document.querySelector('.texto')
+
+    if (inputType === "text") {
+        texto.style.display = "block";
+        seletorData.style.display = "none";
+    } else if (inputType === "selector") {
+        texto.style.display = "none";
+        seletorData.style.display = "block";
+    }
+}
